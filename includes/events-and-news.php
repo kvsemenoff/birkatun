5adb4c5581f15f4b7ba93689c4e5c9952177f45d
engine77 <zaur.ka.00@mail.ru>
2016-11-02 14:58:09 +0300

    1

---
+++ includes/events-and-news.php
@@ -0,0 +1,57 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="df-events-and-news">
+				<div class="df-caption">
+					<h3 class="df-cap">Мероприятия и новости</h3>
+					<span class="df-allevents"><a href="#">Все мероприятия</a></span>
+				</div>
+				<div class="df-content">
+					<div class="df-img">
+						<img src="img/dfpaint.png" alt="" class="img-responsive">
+						<div class="df-img-text">
+							<em>21 сентября 2016</em>
+							<h1>Фестиваль красок</h1>
+							<span>Вход бесплатный. Есть детский фудкорт. Допускается вход со своими напитками и  едой.</span>
+						</div>
+					</div>
+					<div class="df-data-text">
+						<div class="df-data">
+							<span>03 <br> сентября <br> 2016</span>
+						</div>
+						<div class="df-text">
+							<h3 class="df-text-caption">Открытие бассейна «Океан»</h3>
+							<p>Бодуэн дэ Куртенэ в своей основополагающей работе, упомянутой выше, утверждает, что хорей вызывает культурный метаязык. Мелькание мыслей, основываясь на парадоксальном совмещении исключающих друг друга принципов характер
+							</p>
+						</div>
+					</div>
+					<div class="df-data-text">
+						<div class="df-data">
+							<span>29 <br> августа<br> 2016</span>
+						</div>
+						<div class="df-text">
+							<h3 class="df-text-caption">Открытие бассейна «Океан»</h3>
+							<p>Гиперцитата, чтобы уловить хореический ритм или аллитерацию на "л", вероятна. Поэт инстинктивно чувствовал преимущества реального устного исполнения тех стихов, в которых обсценная идиома 
+							</p>
+						</div>
+					</div>
+					<div class="df-data-text">
+						<div class="df-data">
+							<span>23 <br> августа <br> 2016</span>
+						</div>
+						<div class="df-text">
+							<h3 class="df-text-caption">Открытие бассейна «Океан»</h3>
+							<p>Как весело мы провели время и что успели повидать из уст Ивана Петросяна.
+							</p>
+						</div>
+					</div>
+
+				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</div>

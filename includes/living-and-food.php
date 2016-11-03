1068c1297f58f84d0e1939797beaf4946c0c8e78
engine77 <zaur.ka.00@mail.ru>
2016-11-03 13:38:26 +0300

    3

---
+++ includes/living-and-food.php
@@ -0,0 +1,17 @@
+
+
+<div class="df-living">
+	<div class="df-caption">
+		<h3 class="df-cap">Проживание</h3>
+		<span class="df-all-events"><a href="#">Номерной фонд</a></span>
+		<h4 class="df-cap-subtitle">Более 100 номеров различной категории, с 1-2-3-местным размещением. Сравнивайте и выбирайте предложения, подходящее именно для вас!</h4>
+	</div>
+	<div class="df-liv-img">
+		<img src="img/dfimg1.jpg" alt="" class="img-responsive">
+		<div class="df-liv-text">			
+			<h1><a href="#">Люкс Бирюзовая Катунь</a></h1>
+			<span>Гостиная:<br> кресла, журнальный столик, обеденный стол, холодильник, чайник, посуда.</span><br>
+			<span class="df-more">...</span>
+		</div>
+	</div>
+</div>

2c64dfc08ddae8e5e7318c45da6963c00512ba09
hatuhov <hatuhov@yandex.ru>
2016-11-03 14:11:21 +0300

    q

--- includes/entertainment.php
+++ includes/entertainment.php
@@ -4,21 +4,23 @@
 
 <div class="db-wrapper">
 	<div class="db-div1">
-		
+		<span>1</span>
 	</div>
 	<div class="db-div2">
-		
+		<span>2_1</span>
 	</div>
 	<div class="db-div3">
-		
+		<span>2_2</span>
 	</div>
 	<div class="db-div4">
-		
+		<span>2_3</span>
 	</div>
 	<div class="db-div5">
-		
+		<span>3_1</span>
 	</div>
 	<div class="db-div6">
-		
+		<span>3_2</span>
 	</div>
 </div>
+
+<div class="clearfix"></div>

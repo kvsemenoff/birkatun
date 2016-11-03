39f53c5d2b1269afec869eec042736e67df9b679
Berzeg07 <berezg86@gmail.com>
2016-11-03 10:42:58 +0300

    17.197

---
+++ includes/link.php
@@ -0,0 +1,15 @@
+<section class="dd-link">
+	<div class="container my-contain">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="dd-map">
+					dd
+				</div>
+				<div class="dd-form">
+					dd
+				</dd>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</section>
--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 	<script src="libs/wow/wow.min.js"></script>
 	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
 	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+	<script src="https://api-maps.yandex.ru/1.1/index.xml" type="text/javascript"></script>
 
 	<script>
     	new WOW().init();

101304459101adb72a94336f0dac177a18baed20
engine77 <zaur.ka.00@mail.ru>
2016-11-04 11:01:12 +0300

    4

---
+++ includes/food.php
@@ -0,0 +1,43 @@
+<div class="container df-width">	
+	<div class="df-food">
+		<div class="df-caption">
+			<h3 class="df-cap">Питание</h3>
+			<span class="df-all-events"><a href="#">Все места трапез</a></span>			
+		</div>
+		<div class="df-img-slider" id="owl-demo2">
+			<div class="item">
+				<div class="df-food-img">					
+					<img src="img/dffood1.jpg" alt="" class="img-responsive">					
+					<div class="df-food-text">			
+						<h1><a href="#">Ресторан «Бочкари»</a></h1>
+						<span><em>5.3 км от — </em>Бирюзовая катунь</span>
+						<span><em>Средний чек — </em>1200 руб.</span>
+						<a  href="#" class="df-fmore"><em>...</em></a>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="df-food-img">					
+					<img src="img/dffood2.jpg" alt="" class="img-responsive">					
+					<div class="df-food-text">			
+						<h1><a href="#">Ресторан «Бочкари»</a></h1>
+						<span><em>5.3 км от — </em>Бирюзовая катунь</span>
+						<span><em>Средний чек — </em>1200 руб.</span>
+						<a  href="#" class="df-fmore"><em>...</em></a>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="df-food-img">					
+					<img src="img/dffood3.jpg" alt="" class="img-responsive">					
+					<div class="df-food-text">			
+						<h1><a href="#">Ресторан «Бочкари»</a></h1>
+						<span><em>5.3 км от — </em>Бирюзовая катунь</span>
+						<span><em>Средний чек — </em>1200 руб.</span>
+						<a  href="#" class="df-fmore"><em>...</em></a>
+					</div>
+				</div>
+			</div>							
+		</div>
+	</div>
+</div>

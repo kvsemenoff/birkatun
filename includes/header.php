4d41f1d26a75dc1b077473d6a7f44980db271eab
Berzeg07 <berezg86@gmail.com>
2016-11-04 11:55:57 +0300

    17.198

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <section class="dd-header">
-	<div class="container dd-color-header">
-		<div class="col-md-12">
+	<div class="container nopadding dd-color-header">
+		<div class="dd-nav-wrap hidden-sm hidden-xs">
 			<div class="dd-nav">
 				<ul class="dd-mainmenu">
 					<li><a href="#">О парке</a></li>
@@ -25,30 +25,73 @@
 		<div class="clearfix"></div>
 	</div>
 </section>
-<div class="anz-menu">
-    <input type="checkbox" id="check_1" class=""/>
-    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i></label>
-    <div class="anz-perspective">
-        <div class="menu-list-pressed">
-            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i></label>
-            <ul class="dd-mainmenu">
-				<li><a href="#">О парке</a></li>
-				<li><a href="#">Проживание</a></li>
-				<li><a href="#">Питание</a></li>
-				<li><a href="#">Развлечения</a></li>
-				<li><a href="#">Мероприятия</a></li>
-				<li><a href="#">Контакты</a></li>
-			</ul>
-			<div class="dd-phone-wrap">
-				<div class="dd-nav-phone dd-border">
-					<span>бронирование</span><br>
-					<span>+7 (385) 277-89-25</span>
+
+
+
+<!-- <div class="dd-header-top">
+	<section class="dd-header">
+		<div class="container dd-color-header">
+			<div class="col-md-12">
+				<div class="dd-nav">
+					<ul class="dd-mainmenu">
+						<li><a href="#">О парке</a></li>
+						<li><a href="#">Проживание</a></li>
+						<li><a href="#">Питание</a></li>
+						<li><a href="#">Развлечения</a></li>
+						<li><a href="#">Мероприятия</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
 				</div>
-					<div class="dd-nav-phone dd-border2">
-					<span>администрация</span><br>
-					<span>+7 (385) 277-89-25</span>
+				<div class="dd-phone-wrap">
+					<div class="dd-nav-phone dd-border">
+						<span>бронирование</span><br>
+						<span>+7 (385) 277-89-25</span>
+					</div>
+						<div class="dd-nav-phone dd-border2">
+						<span>администрация</span><br>
+						<span>+7 (385) 277-89-25</span>
+					</div>
 				</div>
 			</div>
-        </div>
-    </div>
-</div>
+			<div class="clearfix"></div>
+		</div>
+	</section> -->
+	<div class="anz-menu">
+	    <input type="checkbox" id="check_1" class=""/>
+	    <label class="anz-menu-ch hidden-lg hidden-md" for="check_1"><i class="fa fa-bars" aria-hidden="true"></i></label>
+	    <div class="anz-perspective">
+	        <div class="menu-list-pressed">
+	            <label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i></label>
+	            <ul class="dd-mainmenu">
+					<li><a href="#">О парке</a></li>
+					<li><a href="#">Проживание</a></li>
+					<li><a href="#">Питание</a></li>
+					<li><a href="#">Развлечения</a></li>
+					<li><a href="#">Мероприятия</a></li>
+					<li><a href="#">Контакты</a></li>
+				</ul>
+				<div class="dd-phone-wrap">
+					<div class="dd-nav-phone dd-border">
+						<span>бронирование</span><br>
+						<span>+7 (385) 277-89-25</span>
+					</div>
+						<div class="dd-nav-phone dd-border2">
+						<span>администрация</span><br>
+						<span>+7 (385) 277-89-25</span>
+					</div>
+				</div>
+	        </div>
+	    </div>
+	</div>
+
+
+<section>
+	<div class="container nopadding">
+		<div class="dd-headerimg-wrap">
+			<div class="dd-header-title">
+				Центр семейного отдыха в
+				самом сердце Алтая
+			</div>
+		</div>	
+	</div>
+</section>

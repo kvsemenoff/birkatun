62435bd37ed3f14664e9d6b054219d52bac94f33
Berzeg07 <berezg86@gmail.com>
2016-11-10 10:58:47 +0300

    138

---
+++ includes/header-bg2.php
@@ -0,0 +1,25 @@
+<section>
+	<div class="container nopadding">
+		<div class="dd-headerimg-wrap">
+			<div class="dd-whitelogo">
+				<img class="img-responsive" src="img/dd-logo.jpg" alt="">
+			</div>
+			
+			<div class="dd-header-title">
+			<div class="dd-line">
+				<img class="img-responsive" src="img/dd-line.png" alt="">
+			</div>
+			<!-- <img class="img-responsive" src="img/dd-line.png" alt=""> -->
+				Центр семейного отдыха в<br>
+				самом сердце Алтая
+			</div>
+			<div class="dd-socico">
+				<a href="#"><img src="img/vk2.png" alt=""></a>
+				<a href="#"><img class="dd-k"src="img/face2.png" alt=""></a>
+				<a href="#"><img class="dd-k"src="img/ok2.png" alt=""></a>
+				<a href="#"><img class="dd-k"src="img/inst2.png" alt=""></a>
+				<a href="#"><img class="dd-k"src="img/tube2.png" alt=""></a>
+			</div>
+		</div>	
+	</div>
+</section>
---
+++ includes/parktekst.php
@@ -0,0 +1,26 @@
+<section>
+	<div class="container">
+		<div class="dd-brad-crumps">
+			<a href="#">Главная</a><span>О парке</span>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="container dd-aboutpark-wrap">
+		<div class="dd-content">
+			<div class="dd-title">
+				<span>О парке - текстовая страница</span>
+			</div>
+			<div class="dd-main-txt">
+				<span>
+					Интеграл по поверхности программирует метод последовательных приближений, что и требовалось доказать. Замкнутое множество позитивно усиливает изоморфный множество позитивно усиливает изоморфный натуральный логарифм. Ротор векторного поля обуславливает тригонометрический критерий интегрируемости, что неудивительно.
+				</span>	
+			</div>
+			<div class="owl-carusel">
+				<div class="item">
+				fff
+				</div>
+			</div>	
+		</div>
+	</div>	
+</section>

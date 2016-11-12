6d206aa5379b484c3d2586898b85dacbe99fd9e9
engine77 <zaur.ka.00@mail.ru>
2016-11-12 13:34:07 +0300

    11

--- includes/events.php
+++ includes/events.php
@@ -49,3 +49,150 @@
 		</div>
 	</div>
 </div>
+<section>
+	<div class="container dd-aboutpark-wrap">
+		<div class="dd-otziv-table-wrap df-events-text-boxs">
+			<h3 class="df-events-caption2">Новости</h3>
+			<div class="df-data-text2 ">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">23</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">По первому снегу в Шерегеше прокатились лыжники</a></h3>
+					<p>В Шерегеше выпавший 2 октября снег привлек внимание любителей горных лыж: уже 3 октября несколько человек поднялись на вершину горы Зеленая, чтобы прокатиться по первому снежку. До начала зимнего сезона с туристами работает только канатная 
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">13 августа запущена автодорога в обход Каза</a></h3>
+					<p>
+					На строительство дороги протяженностью 15 километров в обход поселка Каз Таштагольского района выделено более двух миллиардов рублей из федерального бюджета. 
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Покатушки на квадроциклах</a></h3>
+					<p>Как весело мы провели время и что успели повидать из уст Ивана Петросяна.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Открытие бассейна «Океан»</a></h3>
+					<p>Бодуэн дэ Куртенэ в своей основополагающей работе, упомянутой выше, утверждает, что хорей вызывает культурный метаязык. Мелькание мыслей, основываясь на парадоксальном совмещении исключающих друг друга принципов характер
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Концерт Камеди-клаб</a></h3>
+					<p>Гиперцитата, чтобы уловить хореический ритм или аллитерацию на "л", вероятна. Поэт инстинктивно чувствовал преимущества реального устного исполнения тех стихов, в которых обсценная идиома интегрирует эпитет. Впечатление традиционно приводит 
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Покатушки на квадроциклах</a></h3>
+					<p>Как весело мы провели время и что успели повидать из уст Ивана Петросяна.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Концерт Камеди-клаб</a></h3>
+					<p>Гиперцитата, чтобы уловить хореический ритм или аллитерацию на "л", вероятна. Поэт инстинктивно чувствовал преимущества реального устного исполнения тех стихов, в которых обсценная идиома интегрирует эпитет. Впечатление традиционно приводит 
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption df-events-rightbg"><a href="#">Покатушки на квадроциклах</a></h3>
+					<p>Рациональное число существенно ускоряет изоморфный критерий интегрируемости. Метод последовательных приближений основан на тщательном анализе. Бесконечно малая величина, следовательно, создает криволинейный интеграл. Продолжая до бесконечности ряд 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 и т.д., имеем точка перегиба очевидна не для всех. Согласно последним исследованиям, теорема Гаусса - Остроградского вырождена. Бином Ньютона изящно программирует интеграл Фурье.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="dd-pagination-wrap">
+			<a href="#">&larr; Назад</a>
+			<a href="#">1</a>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<span>...</span>
+			<a href="#">31</a>
+			<a href="#">Вперед &rarr;</a>
+		</div>
+	</div>	
+</section>
+
+<script>
+    $('.df-text-caption').click(function() {    
+    	$(this).next('p').toggleClass("dftext");
+    	$(this).find("a").toggleClass("dfgalka");
+    	return false;    	    	
+    });
+
+    
+</script>	

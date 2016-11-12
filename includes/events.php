5a32207e561875d13b0377a11ee313ce3ce6ed22
engine77 <zaur.ka.00@mail.ru>
2016-11-12 09:56:49 +0300

    11

---
+++ includes/events.php
@@ -0,0 +1,51 @@
+<div class="container df-width nopadding">	
+	<div class="df-living">
+		<div class="df-caption">
+			<h3 class="df-cap">Мероприятия</h3>			
+		</div>
+		<div class="df-img-slider" id="owl-demo">
+			<div class="item">
+				<div class="df-liv-img">
+					<div class="df-imgandprice">
+						<img src="img/dd-s.jpg" alt="" class="img-responsive"> 						
+					</div>
+					<div class="df-liv-text df-events2">			
+						<div class="df-img-text">
+							<em class="df-ev2">16 октября 2016</em>
+							<h1><a href="#">Фестиваль красок</a></h1>
+							<span>Вход бесплатный. Есть детский фудкорт. Допускается вход со своими напитками и  едой.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="df-liv-img">
+					<div class="df-imgandprice">
+						<img src="img/df-events1.jpg" alt="" class="img-responsive">						
+					</div>
+					<div class="df-liv-text df-events2">			
+						<div class="df-img-text">
+							<em class="df-ev2">21 октября 2016</em>
+							<h1><a href="#">Формула Знаний</a></h1>
+							<span>Развлекаемся в научном стиле, вместе с музеем интерактивной науки «Ньютон Парк»!</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<div class="df-liv-img">
+					<div class="df-imgandprice">
+						<img src="img/df-events2.jpg" alt="" class="img-responsive"> 					
+					</div>
+					<div class="df-liv-text df-events2">			
+						<div class="df-img-text">
+							<em class="df-ev2">01 ноября 2016</em>
+							<h1><a href="#">Ярмарка здоровья и народной медицины</a></h1>
+							<span>Традиционные и нетрадиционные методы </span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>

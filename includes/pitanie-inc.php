d64034896ef1e584c9eff720c12df0da2a2e1ef4
Berzeg07 <berezg86@gmail.com>
2016-11-12 17:43:46 +0300

    1888

---
+++ includes/pitanie-inc.php
@@ -0,0 +1,92 @@
+<section>
+	<div class="container">
+		<div class="df-brad-crumps">
+			<a href="/">Главная</a>
+			<span>Питание</span>
+		</div>
+	</div>
+</section>
+	<div class="container nopadding back-white">
+		<div class="dd-number-txt-wrap">
+			<h1>Питание</h1>
+			<span>
+				При приготовлении своей продукции мы используем натуральное сырье, без добавления консервантов, растительных белков и усилителей вкуса. Домашние рецепты, и жесткий контроль за качеством продукции, а так же многолетний  опыт и профессионализм поваров и кондитеров — все это позволяет нашей продукции быть  любимой и узнаваемой среди жителей.
+			</span>
+		</div>
+		<div class="dd-wrap-color">
+	 	<div class="dd-nomera">
+	 		<div class="df-liv-img2">
+				<div class="df-imgandprice2">
+					<img src="img/r1.jpg" alt="" class="img-responsive"> 
+					
+				</div>
+				<div class="df-liv-text2">			
+					<h1><a href="#">Ресторан "Бочкари"</a></h1>
+					<span>В меню заведения центральное место занимают оригинальные блюда европейской кухни, а также традиционные блюда русской </span><br>
+					<a  href="#" class="df-more"><em>...</em></a>
+				</div>
+			</div>
+	 	</div>
+	 	<div class="dd-nomera">
+	 		<div class="df-liv-img2">
+				<div class="df-imgandprice2">
+					<img src="img/r2.jpg" alt="" class="img-responsive"> 
+					
+				</div>
+				<div class="df-liv-text2">			
+					<h1><a href="#">Ресторан "Бухта"</a></h1>
+					<span>В меню заведения центральное место занимают оригинальные блюда европейской кухни, а также традиционные блюда русской кухни.</span><br>
+					<a  href="#" class="df-more"><em>...</em></a>
+				</div>
+			</div>
+	 	</div>
+	 	<div class="dd-nomera">
+	 		<div class="df-liv-img2">
+				<div class="df-imgandprice2">
+					<img src="img/r3.jpg" alt="" class="img-responsive"> 
+					
+				</div>
+				<div class="df-liv-text2">			
+					<h1><a href="#">Ресторан "Корабль"</a></h1>
+					<span>В кафе Вы можете пригласить до 25 друзей на семейный вечер, юбилей или корпоративное мероприятие.</span><br>
+					<a  href="#" class="df-more"><em>...</em></a>
+				</div>
+			</div>
+	 	</div>
+	 	<div class="clear"></div>
+
+	 	<div class="dd-bg-color"></div>
+
+	 	<div class="dd-nomera">
+	 		<div class="df-liv-img2">
+				<div class="df-imgandprice2">
+					<img src="img/r4.jpg" alt="" class="img-responsive"> 
+					
+				</div>
+				<div class="df-liv-text2">			
+					<h1><a href="#">Столовая</a></h1>
+					<span>
+					В кафе Вы можете пригласить до 25 друзей на семейный вечер, юбилей или корпоративное мероприятие.
+					</span><br>
+					<a  href="#" class="df-more"><em>...</em></a>
+				</div>
+			</div>
+	 	</div>
+	 	<div class="dd-nomera dd-nomera-line">
+	 		
+	 	</div>
+	 	<div class="dd-nomera">
+	 		
+	 	</div>
+	 	<div class="clear"></div>
+	 	</div>
+	</div>
+</section>
+
+<script>
+    $('.df-open-text').click(function() {    
+    	$(this).prev('p').toggleClass("dftext");
+    	$(this).toggleClass("dfleftright");
+    	return false;    	    	
+    });    
+</script>

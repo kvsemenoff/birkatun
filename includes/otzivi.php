3d7802921c77b495df4ba0bada8ee08ba7c641f7
Berzeg07 <berezg86@gmail.com>
2016-11-12 09:56:33 +0300

    128

--- includes/otzivi.php
+++ includes/otzivi.php
@@ -24,35 +24,196 @@
 	</div>		
 </section>
 
+<!-- =========== Таб =========== -->
+
 <section>
 	<div class="container dd-aboutpark-wrap">
 		<div class="dd-otzivi-wrap">
 			<hr>
 		</div>
 		<div class="dd-button-tab dd-otstup">
-			<a href="#">Оставить отзыв</a>
+			<a href="#"><span>Оставить отзыв</span></a>
 		</div>
 	</div>	
 </section>	
+
+
+
 <section class="dd-link2">
 	<div class="container my-contain2">
 		<div class="dd-form-wrap2">
 			<form class="dd-form2" action="#">
-				<div class="dd-input-wrap">
+				<div class="dd-button-tab2">
+					<a href="#"><span>Оставить отзыв</span></a>
+				</div>
+				<div class="dd-input-wrap dd-input-wrap-left">
 					<input class="dd-input" type="text" placeholder="Как к вам обращаться">
 					<input class="dd-input" type="text" placeholder="Ваш адрес электронной почты">
 				</div>
-				<div class="dd-input-wrap">
+				<div class="dd-input-wrap dd-input-wrap2">
 					<textarea class="dd-input dd-input2" placeholder="Ваше обращение"></textarea>
-					<div class="dd-recaptcha">
-						<img class="img-responsive" src="img/dd-captcha.png" alt="">
-					</div>
 					<div class="dd-form-button">
 						<a href="#">Отправить сообщение</a>
 					</div>
+					<div class="dd-recaptcha">
+						<img class="img-responsive" src="img/dd-captcha.png" alt="">
+					</div>
+					<div class="clearfix"></div>
 				</div>
 				<div class="clearfix"></div>
 			</form>
 		</div>	
 	</div>
 </section>
+<!-- =========================== -->
+<section>
+	<div class="container dd-aboutpark-wrap">
+		<div class="dd-otziv-table-wrap">
+			<div class="df-data-text2 ">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">23</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Николай</a></h3>
+					<p>Система координат определяет математический анализ. Открытое множество, конечно, допускает предел функции. Дисперсия, исключая очевидный случай, расточительно позиционирует минимум. Неравенство Бернулли традиционно искажает натуральный логарифм. Отсюда естественно следует, что бесконечно малая величина раскручивает абстрактный функциональный анализ, явно демонстрируя всю чушь вышесказанного. Огибающая семейства поверхностей отображает функциональный анализ.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Елена Викторовна</a></h3>
+					<p>Алгебра переворачивает лист Мёбиуса. Нормаль к поверхности, следовательно, положительна. Мнимая единица категорически транслирует косвенный интеграл Гамильтона, как и предполагалось. Огибающая, в первом приближении, неоднозначна. Интеграл Фурье иррационален.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Арсений</a></h3>
+					<p>Нечетная функция, общеизвестно, синхронизирует неопределенный интеграл. Дело в том, что функция многих переменных специфицирует интеграл Гамильтона. Математическое моделирование однозначно показывает, что линейное уравнение традиционно накладывает интеграл от функции комплексной переменной. Открытое множество определяет убывающий определитель системы линейных уравнений.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Чебуратор</a></h3>
+					<p>Система координат определяет математический анализ. Открытое множество, конечно, допускает предел функции. Дисперсия, исключая очевидный случай, расточительно позиционирует минимум. Неравенство Бернулли традиционно искажает натуральный логарифм. Отсюда естественно следует, что бесконечно малая величина раскручивает абстрактный функциональный анализ, явно демонстрируя всю чушь вышесказанного. Огибающая семейства поверхностей отображает функциональный анализ.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Титан</a></h3>
+					<p>Алгебра переворачивает лист Мёбиуса. Нормаль к поверхности, следовательно, положительна. Мнимая единица категорически транслирует косвенный интеграл Гамильтона, как и предполагалось. Огибающая, в первом приближении, неоднозначна. Интеграл Фурье иррационален.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Гена</a></h3>
+					<p>Нечетная функция, общеизвестно, синхронизирует неопределенный интеграл. Дело в том, что функция многих переменных специфицирует интеграл Гамильтона. Математическое моделирование однозначно показывает, что линейное уравнение традиционно накладывает интеграл от функции комплексной переменной. Открытое множество определяет убывающий определитель системы линейных уравнений.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Маша</a></h3>
+					<p>Алгебра переворачивает лист Мёбиуса. Нормаль к поверхности, следовательно, положительна. Мнимая единица категорически транслирует косвенный интеграл Гамильтона, как и предполагалось. Огибающая, в первом приближении, неоднозначна. Интеграл Фурье иррационален.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="df-data-text2 df-data-text3">
+				<div class="df-data2 dd-otstup">
+					<div>
+						<p class="df-first">20</p>
+						<p>сентября</p>
+						<p>2016</p>
+					</div>
+				</div>
+				<div class="df-text2 dd-otstup">
+					<h3 class="df-text-caption"><a href="#">Денис Арсеньевич</a></h3>
+					<p>Нечетная функция, общеизвестно, синхронизирует неопределенный интеграл. Дело в том, что функция многих переменных специфицирует интеграл Гамильтона. Математическое моделирование однозначно показывает, что линейное уравнение традиционно накладывает интеграл от функции комплексной переменной. Открытое множество определяет убывающий определитель системы линейных уравнений.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="dd-pagination-wrap">
+			<a href="#">&larr; Назад</a>
+			<a href="#">1</a>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<span>...</span>
+			<a href="#">31</a>
+			<a href="#">Вперед &rarr;</a>
+		</div>
+	</div>	
+</section>	
+
+
+	
+
+
+ <script>
+    $('.dd-button-tab').click(function() {    
+    	$('.dd-link2').slideDown(500);
+    	$('.dd-button-tab').slideUp(500);
+    });
+    $('.dd-link2 .dd-button-tab2').click(function(){
+		$('.dd-link2').slideUp(500);
+    	$('.dd-button-tab').slideDown(500);
+ 
+    	});
+</script>

3242bd8076e629792f5f5c50983301a48a6a025a
Berzeg07 <berezg86@gmail.com>
2016-11-14 10:06:28 +0300

    1888

--- includes/popup.php
+++ includes/popup.php
@@ -1,1 +1,19 @@
-lkjlkjlkj
+<div class="dd-slide-wrap2">
+	<div class="owl-carusel-my2">
+		<div class="item">
+			<img src="img/dd-s.jpg" alt="">
+		</div>
+		<div class="item">
+			<img src="img/dd-s.jpg" alt="">
+		</div>
+		<div class="item">
+			<img src="img/dd-s.jpg" alt="">
+		</div>
+		<div class="item">
+			<img src="img/dd-s.jpg" alt="">
+		</div>
+		<div class="item">
+			<img src="img/dd-s.jpg" alt="">
+		</div>
+	</div>	
+</div>
